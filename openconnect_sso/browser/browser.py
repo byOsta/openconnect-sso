c25fd475c438b662217beca1090df5aad21c19c7
byOsta <jorgeosta04@gmail.com>
2022-03-04 16:49:00 +0100

    Added debugging port to Chrome
    Added debugging port to Chrome to fix "DevToolsActivePort file doesn't exist" error

--- openconnect_sso/browser/browser.py
+++ openconnect_sso/browser/browser.py
@@ -31,7 +31,8 @@ class Browser:
             chrome_options.add_argument("headless")
             chrome_options.add_argument("no-sandbox")
             chrome_options.add_argument("--disable-dev-shm-usage")
-
+            chrome_options.add_argument("--remote-debugging-port=9222")
+            
         if self.proxy:
             proxy = Proxy()
             proxy.proxy_type = ProxyType.MANUAL
